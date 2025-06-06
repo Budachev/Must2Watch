722f8efd2e3f752ed5015c6fe240570de89d5e72
Yevhen Budachov <yevhen.budachov@booking.com>
2025-06-06 14:31:06 +0200

    Fix user createion

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders hello world message', () => {
+    render(<div>Hello, World!</div>);
+    const headingElement = screen.getByText(/Hello, world!/i);
+    expect(headingElement).toBeInTheDocument();
 });
